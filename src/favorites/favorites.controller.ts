8d75e78d013290c03fda1a9b53662a807f7ff703
r1soX <vladislav-smolin@list.ru>
2023-12-25 09:46:07 +0700

    Update favorites.controller.ts

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,16 +1,16 @@
 import { Controller, Post, Body, Get, Query, UseGuards } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { GetCurrentUserId } from 'src/common/decorators';
-import { AuthGuard } from 'src/auth/auth.guard';
+import { JwtAuthGuard } from 'src/auth/guards/auth.guard';
 
 @Controller('favorites')
-@UseGuards(AuthGuard)
+@UseGuards(JwtAuthGuard)
 export class FavoritesController {
   constructor(private favoritesService: FavoritesService) {}
 
   @Get('/check')
-  async checkOne(@GetCurrentUserId() userId: string, @Query('id') id: number) {
-    return this.favoritesService.checkOne(userId, id);
+  async check(@GetCurrentUserId() userId: string, @Query('id') id: number) {
+    return this.favoritesService.check(userId, id);
   }
 
   @Get()
